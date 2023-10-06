9961411aa50af983ed55fdeb6d62d5669aa6809d
tarekbouamer <tarekbouamer1788@gmail.com>
2023-10-06 08:55:42 +0400

    transform init

---
+++ core/transforms/__init__.py
@@ -0,0 +1,3 @@
+from .tfn import ToTensor, make_transforms, tfn_grayscale, tfn_image_net
+
+__all__ = ["make_transforms", "tfn_image_net", "tfn_grayscale", "ToTensor"]
---
+++ core/transforms/tfn.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import torch
+from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
+from torchvision import transforms as tfn
+
+tfn_image_net = tfn.Normalize(mean=IMAGENET_DEFAULT_MEAN,
+                              std=IMAGENET_DEFAULT_STD)
+
+tfn_grayscale = tfn.Grayscale()
+
+
+class ToTensor:
+    def __init__(self, dtype=torch.float32):
+        self.dtype = dtype
+
+    def __call__(self, np_img):
+        if np_img.ndim < 3:
+            np_img = np.expand_dims(np_img, axis=-1)
+
+        np_img = np_img.transpose((2, 0, 1))
+        tensor_img = torch.from_numpy(np_img).to(dtype=self.dtype)
+        return tensor_img
+
+
+def make_transforms(normalize="imagenet", gray=False):
+
+    # to tensor
+    tfl = []
+
+    # grayscale
+    if gray:
+        tfl += [tfn_grayscale]
+
+    # normalize
+    if normalize == "imagenet":
+        tfl += [tfn_image_net]
+    else:
+        pass
+
+    # return transforms
+    return tfn.Compose(tfl)
